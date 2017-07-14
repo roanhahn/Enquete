80e65d35d4bd06565059cf9b531976e1983fbba4
Roan Hahn <roan@digitalmonkey.nl>
2017-07-14 14:12:04 +0200

    Carousel 2

--- src/static/js/99-functions.js
+++ src/static/js/99-functions.js
@@ -4,39 +4,64 @@
 
 $(document).ready(function() {
 
-	var owl = $("#slider");
+	$('#slider').owlCarousel({
+	    loop:false,
+	    margin:0,
+	    nav:false,
+	    dots:false,
+	    callbacks: true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	})
 
-	owl.owlCarousel({
-		  items : 1, //10 items above 1000px browser width
-		  itemsDesktop : [1000,1], //5 items between 1000px and 901px
-		  itemsDesktopSmall : [900,1], // betweem 900px and 601px
-		  itemsTablet: [600,1], //2 items between 600 and 0
-		  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-		  pagination: false,
-		  responsive: true,
-		  autoHeight: true,
-		  slideSpeed: 400,
-		  paginationSpeed: 400,
-		  rewindNav: false,
-		  responsiveRefreshRate: 10,
-		  touchDrag: false,
-		  mouseDrag: false
+	var owl = $('#slider');
+	// Go to the next item
+	$('.next').click(function() {
+	    owl.trigger('next.owl.carousel');
+	})
 
-		});
+	// var owl = $("#slider");
 
-	  // Custom Navigation Events
-	  $(".next").click(function(){
-	  	owl.trigger('owl.next');
-	  })
-	  $(".prev").click(function(){
-	  	owl.trigger('owl.prev');
-	  })
-	  $(".play").click(function(){
-		owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-	  })
-	  $(".stop").click(function(){
-	  	owl.trigger('owl.stop');
-	  })
+	// owl.owlCarousel({
+	// 	  items : 1, //10 items above 1000px browser width
+	// 	  itemsDesktop : [1000,1], //5 items between 1000px and 901px
+	// 	  itemsDesktopSmall : [900,1], // betweem 900px and 601px
+	// 	  itemsTablet: [600,1], //2 items between 600 and 0
+	// 	  itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+	// 	  pagination: false,
+	// 	  responsive: true,
+	// 	  autoHeight: true,
+	// 	  slideSpeed: 400,
+	// 	  paginationSpeed: 400,
+	// 	  rewindNav: false,
+	// 	  responsiveRefreshRate: 10,
+	// 	  touchDrag: false,
+	// 	  mouseDrag: false
+
+	// 	});
+
+	//   // Custom Navigation Events
+	//   $(".next").click(function(){
+	//   	owl.trigger('owl.next');
+	//   })
+	//   $(".prev").click(function(){
+	//   	owl.trigger('owl.prev');
+	//   })
+	//   $(".play").click(function(){
+	// 	owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+	//   })
+	//   $(".stop").click(function(){
+	//   	owl.trigger('owl.stop');
+	//   })
 
 
 	  // ANSWERS
